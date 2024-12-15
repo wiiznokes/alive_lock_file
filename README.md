bd6b6401f64577892039ac1f2ba48a712d4457a7
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-12-15 21:35:01 +0100

    v2

--- README.md
+++ README.md
@@ -4,24 +4,7 @@
 [![docs.rs](https://img.shields.io/badge/docs.rs-alive_lock_file-blue?style=flat-square&logo=docs.rs)](https://docs.rs/alive_lock_file)
 [![license](https://img.shields.io/badge/license-MIT-blue?style=flat-square)](#license)
 
-A simple crate to create lock files without creating dead locks. For that, it use 2 things:
+## Feature
+
 - join the provided path to the `$XDG_RUNTIME_DIR` env variable. This directory get cleanned automatically by the system, and is mount as a ramfs.
-- intercept signals
-
-## Usage
-
-```rs
-use alive_lock_file::{init_signals, LockFileState};
-
-fn main() {
-    // intercept the `SIGINT` and `SIGTERM` signals.
-    init_signals();
-
-    match LockFileState::try_lock("file.lock").unwrap() {
-        LockFileState::Lock(_lock) => {
-            // while _lock is in scope, `file.lock` will not be removed
-        }
-        LockFileState::AlreadyLocked => {}
-    };
-}
-```
+- Atomic file creation
--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,72 @@
-//! A simple crate to create lock files without creating dead locks
-//!
-//! ```rs
-//! use alive_lock_file::LockFileState;
-//!
-//! fn main() {
-//!     match LockFileState::try_lock("file.lock").unwrap() {
-//!         LockFileState::Lock(_lock) => {
-//!             // while _lock is in scope, `file.lock` will not be removed
-//!         }
-//!         LockFileState::AlreadyLocked => {}
-//!     };
-//! }
-//! ```
-
 use std::{
-    collections::HashSet,
     fs::{self, File},
     io::ErrorKind,
     path::{Path, PathBuf},
-    sync::{LazyLock, Mutex},
 };
 
 use anyhow::{anyhow, Result};
+use log::error;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum LockFileState {
-    Lock(Lock),
+#[must_use]
+pub enum LockResult {
+    Success,
     AlreadyLocked,
 }
 
-/// Represent a lock file. When this value is dropped, the corresponding lock file
-/// will be removed.
+#[must_use]
+pub enum LockResultWithDrop {
+    Locked(Lock),
+    AlreadyLocked,
+}
+
+impl LockResultWithDrop {
+    pub fn has_lock(&self) -> bool {
+        matches!(self, Self::Locked(_))
+    }
+}
+
+/// Represent a lock file. When this value is dropped, the corresponding lock file will be removed.
 #[derive(Debug, Clone, PartialEq, Eq)]
+#[must_use]
 pub struct Lock {
     path: PathBuf,
 }
 
-static FILE_PATHS: LazyLock<Mutex<HashSet<PathBuf>>> = LazyLock::new(|| Mutex::new(HashSet::new()));
+/// Remove the lock if exist. Return true if successfully removed, false if there was no lock.
+pub fn remove_lock<S: AsRef<str>>(name: S) -> Result<bool> {
+    let path = get_lock_path(name.as_ref())?;
 
-impl LockFileState {
-    /// Try to acquire a lock. The name provided will be join the the runtime dir of the platform.
-    /// On unix, it will be `$XDG_RUNTIME_DIR`.
-    pub fn try_lock<S: AsRef<str>>(name: S) -> Result<LockFileState> {
-        let path = dirs::runtime_dir()
-            .ok_or(anyhow!("no runtime dir"))?
-            .join(name.as_ref());
-
-        let parents = path.parent().ok_or(anyhow!("no parent directory"))?;
-
-        std::fs::create_dir_all(parents)?;
-
-        match File::create_new(&path) {
-            Ok(_) => {}
-            Err(e) => {
-                if e.kind() == ErrorKind::AlreadyExists {
-                    return Ok(LockFileState::AlreadyLocked);
-                }
-                return Err(e.into());
-            }
-        }
-
-        FILE_PATHS.lock().unwrap().insert(path.clone());
-
-        Ok(LockFileState::Lock(Lock { path }))
+    if fs::exists(&path)? {
+        fs::remove_file(&path)?;
+        Ok(true)
+    } else {
+        Ok(false)
     }
+}
 
-    pub fn has_lock(&self) -> bool {
-        match self {
-            LockFileState::Lock(_) => true,
-            LockFileState::AlreadyLocked => false,
-        }
-    }
+/// Try to acquire the lock.
+pub fn try_lock<S: AsRef<str>>(name: S) -> Result<LockResult> {
+    let path = get_lock_path(name.as_ref())?;
+    let res = create_log_file(&path)?;
+    Ok(res)
+}
+
+/// Return true if this name is locked.
+pub fn is_locked<S: AsRef<str>>(name: S) -> Result<bool> {
+    let path = get_lock_path(name.as_ref())?;
+    let exist = fs::exists(&path)?;
+    Ok(exist)
+}
+
+/// Try to acquire the lock, and unlock when the [`Lock`] is dropped.
+pub fn try_lock_until_dropped<S: AsRef<str>>(name: S) -> Result<LockResultWithDrop> {
+    let path = get_lock_path(name.as_ref())?;
+    let res = create_log_file(&path)?;
+    let res = match res {
+        LockResult::Success => LockResultWithDrop::Locked(Lock { path }),
+        LockResult::AlreadyLocked => LockResultWithDrop::AlreadyLocked,
+    };
+    Ok(res)
 }
 
 impl Lock {
@@ -82,8 +78,35 @@ impl Lock {
 
 impl Drop for Lock {
     fn drop(&mut self) {
-        _ = fs::remove_file(&self.path);
+        if let Err(e) = fs::remove_file(&self.path) {
+            error!(
+                "can't remove file {} in drop lock: {e}",
+                self.path.display()
+            );
+        }
+    }
+}
 
-        FILE_PATHS.lock().unwrap().remove(&self.path);
+fn get_lock_path(name: &str) -> Result<PathBuf> {
+    let path = dirs::runtime_dir()
+        .ok_or(anyhow!("no runtime dir"))?
+        .join(name);
+
+    Ok(path)
+}
+
+fn create_log_file(path: &Path) -> Result<LockResult> {
+    let parents = path.parent().ok_or(anyhow!("no parent directory"))?;
+
+    std::fs::create_dir_all(parents)?;
+
+    match File::create_new(&path) {
+        Ok(_) => Ok(LockResult::Success),
+        Err(e) => {
+            if e.kind() == ErrorKind::AlreadyExists {
+                return Ok(LockResult::AlreadyLocked);
+            }
+            return Err(e.into());
+        }
     }
 }
